0f13f852ea94885de54a2aa4b1403a0535e103ab
Maxim Sherstkov <maxim.sherstkov@saritasa.com>
2024-07-03 10:52:28 +0700

    Add basic html & text utility, fix assets folder JC-604

--- apps/vanilla/vite.config.ts
+++ apps/vanilla/vite.config.ts
@@ -7,23 +7,23 @@ const outDir = resolve(__dirname, '../../dist/apps/vanilla');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root,
-  plugins: [],
-  resolve: {
-    alias: {
-      '@js-camp/vanilla': resolve(__dirname, 'src'),
-      '@js-camp': resolve(__dirname, '../../libs'),
-    },
-  },
-  build: {
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: [
-        resolve(root, 'index.html'),
-        resolve(root, 'example', 'index.html'),
-        resolve(root, 'example', 'nested', 'index.html'),
-      ],
-    },
-  },
+	root,
+	plugins: [],
+	resolve: {
+		alias: {
+			'@js-camp/vanilla': resolve(__dirname, 'src'),
+			'@js-camp': resolve(__dirname, '../../libs'),
+		},
+	},
+	build: {
+		outDir,
+		emptyOutDir: true,
+		rollupOptions: {
+			input: [
+				resolve(root, 'index.html'),
+				resolve(root, 'example', 'index.html'),
+				resolve(root, 'example', 'nested', 'index.html'),
+			],
+		},
+	},
 });
