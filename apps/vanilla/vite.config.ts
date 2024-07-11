80013885970bc9a3ca78f8b9482110dbd2074eff
Maxim Sherstkov <maxim.sherstkov@saritasa.com>
2024-07-11 10:15:56 +0700

    Refactor directory organization JC-604

--- apps/vanilla/vite.config.ts
+++ apps/vanilla/vite.config.ts
@@ -19,7 +19,12 @@ export default defineConfig({
 		outDir,
 		emptyOutDir: true,
 		rollupOptions: {
-			input: [resolve(root, 'index.html')],
+			input: [
+				resolve(root, 'index.html'),
+				resolve(root, 'book-catalog', 'index.html'),
+				resolve(root, 'blackjack', 'index.html'),
+
+			],
 		},
 	},
 });
