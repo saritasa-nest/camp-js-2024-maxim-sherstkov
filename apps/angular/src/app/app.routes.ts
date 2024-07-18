3da762b007ee105141e1b8579fdd29abff77fe4c
Maxim Sherstkov <maxim.sherstkov@saritasa.com>
2024-07-18 17:17:58 +0700

    Add lazy loading for dashboard component JC-642

--- apps/angular/src/app/app.routes.ts
+++ apps/angular/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
-import { AnimeDashboardComponent } from './features/anime-dashboard/anime-dashboard.component';
-
 /** Routes object. */
 export const appRoutes: Routes = [
 	{
 		path: '',
-		component: AnimeDashboardComponent,
+		loadComponent: () => import('./features/anime-dashboard/anime-dashboard.component').then(
+			c => c.AnimeDashboardComponent,
+		),
 	},
 ];
