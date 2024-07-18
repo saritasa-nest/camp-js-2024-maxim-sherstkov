2e1d93135535c64a1255a6d8b21c592c8cf22e58
Maxim Sherstkov <maxim.sherstkov@saritasa.com>
2024-07-18 18:31:59 +0700

    Refactor asyncPipe for animeList JC-642

--- apps/angular/src/app/features/anime-dashboard/components/anime-table/anime-table.component.ts
+++ apps/angular/src/app/features/anime-dashboard/components/anime-table/anime-table.component.ts
@@ -2,8 +2,6 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { AnimeService } from '@js-camp/angular/core/services/anime.service';
-import { Observable } from 'rxjs';
-import { Anime } from '@js-camp/core/models/anime';
 
 /** Table of anime list component. */
 @Component({
@@ -18,12 +16,9 @@ export class AnimeTableComponent {
 	private readonly animeService: AnimeService = inject(AnimeService);
 
 	/** List of anime from the API. */
-	protected readonly animeList$: Observable<readonly Anime[]>;
-
-	public constructor() {
-		this.animeList$ = this.animeService.getAnimeList();
-	}
+	protected readonly animeList$ = this.animeService.getAnimeList();
 
 	/** Columns names. */
+	// TODO make displayedColumns typesafed
 	protected readonly displayedColumns = ['image', 'titleEnglish', 'titleJapanese', 'airedStart', 'type', 'status'] as const;
 }
