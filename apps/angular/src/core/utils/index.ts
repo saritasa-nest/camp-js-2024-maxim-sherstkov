f6e1319c9ecc2097f5a225a965e12e721006d63f
Maxim Sherstkov <maxim.sherstkov@saritasa.com>
2024-07-29 18:10:34 +0700

    Delete unused utility function JC-659

--- apps/angular/src/core/utils/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Get number from value or return null.
- * @param value Value to be checked.
- */
-export function getNumberFromString(value: string | null): number | null {
-	const possibleNumber = Number(value);
-	return possibleNumber ? possibleNumber : null;
-}
