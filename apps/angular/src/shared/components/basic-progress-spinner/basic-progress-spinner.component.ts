94bd42cadf815ce723903cd8c84990b52bd9dc10
Maxim Sherstkov <maxim.sherstkov@saritasa.com>
2024-07-29 17:35:06 +0700

    Refactor basic spinner, binding for table JC-642

--- apps/angular/src/shared/components/basic-progress-spinner/basic-progress-spinner.component.ts
+++ apps/angular/src/shared/components/basic-progress-spinner/basic-progress-spinner.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
@@ -11,7 +11,4 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	imports: [CommonModule, MatProgressSpinnerModule],
 })
-export class BasicProgressSpinnerComponent {
-	/** Value for a top and bottom padding. */
-	@Input() public padding = '0px';
-}
+export class BasicProgressSpinnerComponent {}
