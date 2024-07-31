939b1c7c6ab8807db0cd9f3026acc8c36cc0906e
Maxim Sherstkov <maxim.sherstkov@saritasa.com>
2024-07-31 16:14:24 +0700

    Refactor login validation JC-678

--- apps/angular/src/core/services/api-url.service.ts
+++ apps/angular/src/core/services/api-url.service.ts
@@ -19,6 +19,6 @@ export class ApiUrlService {
 
 	public constructor() {
 		this.animeListPath = `${this.appConfig.apiUrl}/anime/anime/`;
-		this.loginPath = `${this.appConfig.apiUrl}/auth/login`;
+		this.loginPath = `${this.appConfig.apiUrl}/auth/login/`;
 	}
 }
